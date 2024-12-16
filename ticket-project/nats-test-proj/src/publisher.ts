d2a5085ef1a179202afa23daca044f19b79a620a
Vlada Minina <04vlada07@gmail.com>
2024-12-16 12:25:11 +0200

    added simple publisher instance

---
+++ ticket-project/nats-test-proj/src/publisher.ts
@@ -0,0 +1,24 @@
+import nats from 'node-nats-streaming';
+
+const stan = nats.connect('ticketing', 'abc', {
+    url: 'http://localhost:4222',
+});
+
+stan.on('connect', () => {
+    console.log("Publisher connected to NATS");
+
+    const data = {
+        id: '12',
+        title: 'ticket',
+        price: 12
+    };
+
+    const json = JSON.stringify(data);
+
+    stan.publish('ticket:created', json, () => {
+        console.log(
+            "ticket:created data was published!"
+        );
+    })
+})
+
