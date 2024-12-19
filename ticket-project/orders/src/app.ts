6c5f185144843260322d6e8519c90305d8e8b934
Vlada Minina <04vlada07@gmail.com>
2024-12-19 15:48:36 +0200

    added Order model with issue

--- ticket-project/orders/src/app.ts
+++ ticket-project/orders/src/app.ts
@@ -4,10 +4,10 @@ import cookieSession from 'cookie-session';
 
 import { errorHandler,NotFoundError, currentUserMid } from '@vm-kvitki/common-lib';
 import 'express-async-errors';
-import { deleteOrderRouter } from '../routes/delete';
-import { showOrderRouter } from '../routes/show';
-import { indexOrderRouter } from '../routes';
-import { newOrderRouter } from '../routes/new';
+import { deleteOrderRouter } from './routes/delete';
+import { showOrderRouter } from './routes/show';
+import { indexOrderRouter } from './routes';
+import { newOrderRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true); //use it cause you are using express ingress-injnex
