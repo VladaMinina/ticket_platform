6ec3e2024967edc1ee73d9d3b648bb1ab8531eab
Vlada Minina <04vlada07@gmail.com>
2025-01-04 15:13:54 +0200

    fixed client feth

--- ticket-project/client/pages/index.js
+++ ticket-project/client/pages/index.js
@@ -1,30 +1,15 @@
-import buildClient from '../api/build-client';
+import buildClient from "../api/build-client";
 
-const Landing = ({currentUser}) => {
-    
-    return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>
-}
+const Landing = ({ currentUser }) => {
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
+};
 //contecst -> add in addition headers and others props from the browser
 Landing.getInitialProps = async (context) => {
-    //console.log(context.req ? context.req.headers : 'No headers');
-    console.log("Landing page");
-    return buildClient(context)
-        .get('/api/users/currentuser')
-        .then((res) => res.data) // Extract and return the data
-        .catch((err) => {
-            console.error('Error fetching current user on landing page');
-            return { currentUser: null }; // Fallback response
-        });
-    // let response;
-    // try {
-    //     const client = buildClient(context);
-    //     const res = await client.get('/api/users/currentuser');
-    //     response = res.data;
-    // } catch (err) {
-    //     console.error('Error fetching current user:', err);
-    //     response = { currentUser: null };
-    // }
-    // return response;
+  return {};
 };
 
 export default Landing;
